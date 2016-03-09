e7166d4dbd9f828b6d732cc973513075d5d28874
Reinhard Vicinus <r.vicinus@metaways.de>
2016-03-09 17:48:29 +0100

    removed auto update service

--- manifests/service.pp
+++ manifests/service.pp
@@ -23,13 +23,4 @@ class yum::service {
     path        => '/bin:/sbin:/usr/bin:/usr/sbin',
     refreshonly => true
   }
-
-  if $yum::autoUpdate == 'yes' {
-    service {
-      $yum::params::serviceAutoUpdate:
-      ensure  => 'running',
-      enable  => true,
-      require => Package[$yum::params::packageAutoUpdate];
-    }
-  }
 }
